d61509a435d72eb05f9468e1a6fa09e2098c2fd3
EKreir <155242713+EKreir@users.noreply.github.com>
2024-05-06 18:56:19 +0200

    Update form.php

--- MVC/views/form.php
+++ MVC/views/form.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <h1>Login</h1>
-        <form action="<?php echo BASE_URL.'/profile'; ?>" method="post">
+        <form action="<?php echo BASE_URL.'/profile.php'; ?>" method="post">
         <label for="email">Email:</label>
         <input type="email" name="email" id="email" required><br><br>
         <label for="password">Password:</label>
