728dabe6ec4c612a1ed32d0fc7b65a42f829872b
EKreir <155242713+EKreir@users.noreply.github.com>
2024-05-06 18:58:50 +0200

    Update profileController.php

--- MVC/controllers/profileController.php
+++ MVC/controllers/profileController.php
@@ -1,9 +1,9 @@
 <?php
-require_once './home/eliess/studiwork/MVC/models/user.php';
+require_once 'models/user.php';
 class ProfileController {
    public function index() {
       if($_SERVER['REQUEST_METHOD'] === "POST") {
-         require_once './home/eliess/studiwork/MVC/views/profile.php';
+         require_once 'views/profile.php';
       }
    }
 }
